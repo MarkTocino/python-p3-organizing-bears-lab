f158e4841faeaba287391b225ee96759411e9dd8
MarkTocino <Mark_tocino@yahoo.com>
2023-08-06 12:38:45 -0400

    Compelted SQL

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr.Chocolate', 12, 'M', 'Brown', 'Angry', 1),
+('Rowdy', 9, 'F', 'Brown', 'Angry', 0),
+('Tabitha', 92, 'F', 'Brown', 'Angry', 1),
+('Sergeant Brown', 19, 'M', 'Brown', 'Angry', 0),
+('Melissa', 20, 'F', 'Brown', 'Angry', 1),
+('Grinch', 13, 'M', 'Brown', 'Angry', 0),
+('Wendy', 24, 'M', 'Brown', 'Angry', 1),
+(NULL , 24, 'M', 'Brown', 'Angry', 1);
